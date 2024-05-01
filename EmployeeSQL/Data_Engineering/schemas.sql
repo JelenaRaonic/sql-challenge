0b9c5ced9a82fcfef24d7b31da98898d3b385e58
Jelena Raonic <jelenaraonic2@gmail.com>
2024-05-01 11:22:28 -0400

    add results

--- EmployeeSQL/Data_Engineering/schemas.sql
+++ EmployeeSQL/Data_Engineering/schemas.sql
@@ -1,4 +1,4 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+ -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
@@ -11,7 +11,7 @@ CREATE TABLE "departments" (
 );
 
 CREATE TABLE "employees" (
-    "emp_no" char(5)   NOT NULL,
+    "emp_no" int   NOT NULL,
     "emp_title_id" char(5)   NOT NULL,
     "birth_date" date   NOT NULL,
     "first_name" varchar(20)   NOT NULL,
@@ -23,21 +23,6 @@ CREATE TABLE "employees" (
      )
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" char(4)   NOT NULL
-);
-
-CREATE TABLE "dept_menager" (
-    "dept_no" char(4)   NOT NULL,
-    "emp_no" int   NOT NULL
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
-);
-
 CREATE TABLE "titles" (
     "title_id" char(5)   NOT NULL,
     "title" varchar(20)   NOT NULL,
@@ -46,20 +31,35 @@ CREATE TABLE "titles" (
      )
 );
 
+CREATE TABLE "dept_menager" (
+    "dept_no" char(4)   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" char(4)   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_menager" ADD CONSTRAINT "fk_dept_menager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_menager" ADD CONSTRAINT "fk_dept_menager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
@@ -93,4 +93,3 @@ SELECT * FROM dept_menager;
 --imported salaries.csv to salaries table
 --check if the data is imported correctly
 SELECT * FROM salaries;
-
